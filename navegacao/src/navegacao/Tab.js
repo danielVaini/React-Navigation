704b35076a385f90ef91b88bfb4ab745b21685db
Daniel Santos <dnlvaini@gmail.com>
2020-11-13 15:31:36 -0300

    Terminando navegação
    Testando  três métodos de navegão no React-Native

---
+++ navegacao/src/navegacao/Tab.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import TelaA from '../views/TelaA';
+import TelaB from '../views/TelaB';
+import TelaC from '../views/TelaC';
+
+
+
+const Tab = createBottomTabNavigator();
+
+export default props => (
+  <Tab.Navigator
+  screenOptions={({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+
+      switch (route.name) {
+        case 'TelaA':
+          iconName = focused
+          ? 'apps-outline'
+          : 'ios-information-circle-outline';
+          break;
+
+        case 'TelaB':
+          iconName = focused
+            ? 'apps-outline'
+            : 'ios-information-circle-outline';
+          break;
+
+        case 'TelaC':
+          iconName = focused ? 'list-outline' : 'list-circle-outline';
+          break;
+      }
+
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+  })}
+  tabBarOptions={{
+    activeTintColor: 'red',
+    inactiveTintColor: 'blue',
+    showLabel: true,
+   // labelStyle: {fontSize: 30 },
+  }} initialRouteName="TelaB">
+    <Tab.Screen  name="TelaA" component={TelaA} options={{title: 'Home'}} />
+    <Tab.Screen  name="TelaB" component={TelaB} options={{title: 'Config'}}/>
+    <Tab.Screen  name="TelaC" component={TelaC} options={{title: 'Initial '}}/>
+  </Tab.Navigator>
+);
